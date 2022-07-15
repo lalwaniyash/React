fb54c0fec74839834c5c001debdd2856ba06206f
lalwaniyash <77988031+lalwaniyash@users.noreply.github.com>
2022-07-15 23:40:20 +0530

    Day-1 Starting JSX

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//ReactDOM.render(WHAT TO Shared, WHERE TO SHOW IT)
+ReactDOM.render(
+  <div>
+    <h1>Hello world!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
